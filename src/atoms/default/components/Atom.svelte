1160cd9b13658ab2cbee8f64f542d1fe94058ceb
Niels de Hoog <njdehoog@gmail.com>
2023-12-19 10:56:04 +0000

    Initial commit

---
+++ src/atoms/default/components/Atom.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { windowInnerWidth, windowInnerHeight } from '$lib/stores/dimensions.js';
+
+  export let name = "atom";
+</script>
+
+<svelte:window bind:innerWidth={$windowInnerWidth} bind:innerHeight={$windowInnerHeight} />
+
+<div class="atom">
+  <h2>Hello {name}</h2>
+</div>
+
+<style lang="scss">
+  h2 {
+    @include f-headline();
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+</style>
