f49f194982dffad4a801ba96e3ca3e8bbf3b0898
Niels de Hoog <njdehoog@gmail.com>
2023-12-20 12:28:12 +0000

    Align full width immersive atom

--- src/atoms/default/app.js
+++ src/atoms/default/app.js
@@ -1,7 +1,7 @@
 import './styles/main.scss';
 import { selectorAppOrDCR } from '$lib/helpers/util';
 import Atom from './components/Atom.svelte';
-import HeaderImage from './components/HeaderImage.svelte';
+import MainMedia from './components/MainMedia.svelte';
 
 // create scrollbar width CSS variable
 import '$lib/helpers/scrollbarWidth';
@@ -11,7 +11,7 @@ const mainMedia = document.createElement("div");
 mainMedia.classList.add("drone-footage");
 headerContainer.prepend(mainMedia);
 
-const headerImage = new HeaderImage({
+const headerImage = new MainMedia({
   target: mainMedia,
   hydrate: true,
   props: {},
@@ -21,11 +21,11 @@ const articleHeader = document.querySelector(selectorAppOrDCR("articleHeader"));
 const bylineElement = document.querySelector(selectorAppOrDCR("byline"));
 articleHeader.append(bylineElement);
 
-// const app = new Atom({
-// 	target: document.getElementById('gv-atom'),
-// 	hydrate: true,
-// 	props: {},
-// });
+const app = new Atom({
+	target: document.getElementById('gv-atom'),
+	hydrate: true,
+	props: {},
+});
 
 export default {
 	headerImage,
