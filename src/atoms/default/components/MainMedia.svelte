9a930c80a25bef93662f778a7496980da39bd66f
Niels de Hoog <njdehoog@gmail.com>
2024-01-28 14:46:34 +0000

    Pause main video when hidden

--- src/atoms/default/components/MainMedia.svelte
+++ src/atoms/default/components/MainMedia.svelte
@@ -1,15 +1,33 @@
 <script>
   import { isMobile } from "$lib/stores/devices";
+  import { onMount } from "svelte";
 
+  let video;
+ 
   $: poster = $isMobile ? "https://uploads.guim.co.uk/2024/01/10/Al_Zahra-short-mobile.jpg" : "https://uploads.guim.co.uk/2024/01/10/Al_Zahra_site.00_00_01_24.Still001.jpg";
   $: src = $isMobile ? "https://uploads.guim.co.uk/2024/01/10/alZahraShortMobile.mp4" : "https://uploads.guim.co.uk/2024/01/10/Al_Zahra-short.mp4";
+
+  onMount(() => {
+    let observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting && !video.paused) {
+                    video.pause();
+                } else if (video.paused) {
+                    video.play();
+                }
+            });
+        },
+        { threshold: 0.2 }
+    );
+    observer.observe(video);
+  })
 </script>
 
 <div class="main-media">
   <video
+    bind:this={video}
     class="uit-video uit-desktop"
     playsinline
-    autoplay
     muted
     loop
     preload="none"
