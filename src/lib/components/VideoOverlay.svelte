9291c62ebc5b77ca79dea3b8714c3b848533ac3c
Niels de Hoog <njdehoog@gmail.com>
2024-01-28 13:38:12 +0000

    Fix aspect ratio for video overlay

--- src/lib/components/VideoOverlay.svelte
+++ src/lib/components/VideoOverlay.svelte
@@ -1,17 +1,34 @@
 <script>
     import VideoPlayer from "$lib/components/VideoPlayer.svelte";
-    import AspectRatioBox from "$lib/components/AspectRatioBox.svelte";
 
     export let video;
 
     $: isPortrait = video.orientation && video.orientation === "portrait";
+    $: aspectRatio = isPortrait ? 1.7792 : 0.5625;
+
+    let containerSize = {width: 0, height: 0};
+
+    let width = 0, height = 0;
+    function updateSize(aspectRatio, containerSize) {
+        const containerAspectRatio = containerSize.height / containerSize.width;
+
+        if (aspectRatio > containerAspectRatio) {
+            width = containerSize.height / aspectRatio;
+            height = containerSize.height;
+        } else {
+            height = containerSize.width * aspectRatio;
+            width = containerSize.width;
+        }
+    }
+    
+    $: updateSize(aspectRatio, containerSize)
 </script>
 
 <div class="video-overlay">
-    <div class="video-wrapper">
-        <AspectRatioBox aspectRatio={isPortrait ? 1.7792 : 0.5625 }>
+    <div class="video-wrapper" bind:clientWidth={containerSize.width} bind:clientHeight={containerSize.height}>
+        <div class="aspect-ratio" style="width: {width}px; height: {height}px;">
             <VideoPlayer src={video.src} posterImage={video.posterImage} />
-        </AspectRatioBox>
+        </div>
     </div>
 </div>
 
@@ -21,18 +38,25 @@
         display: flex;
         flex-direction: row;
         justify-content: center;
-        align-items: center;
+        align-items: stretch;
     }
 
     .video-wrapper {
+        position: relative;
         flex-basis: 100%;
-        margin: 0 10px;
-        max-width: 1024px;
-        background-color: #FFF;
+        margin: 10px;
+        max-width: 100%;
 
         @include mq($from: tablet) {
             flex-basis: 80%;
             margin: 0;
         }
+    }
+
+    .aspect-ratio {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
